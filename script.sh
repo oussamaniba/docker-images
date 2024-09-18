cb26a2b2ec8db697f280ae94f7b5e6e31a966ab0
Oussama Aniba <o.aniba@cirestechnologies.ma>
2024-09-18 16:07:32 +0100

    change

--- script.sh
+++ script.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Exit immediately if any command exits with a non-zero status
+set -e
+
 # Check if the number of arguments is less than 2 or more than 3
 if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
-    echo "Usage: $0 <repository_url> <destination_folder> [use_env_file]"
+    echo "Usage: $0 <repository_url> <destination_folder> [env_file]"
     exit 1
 fi
 
 # Get the repository URL and destination folder from command-line arguments
 repo_url="$1"
 destination_folder="$2"
+
+# Optional: Get the environment file from the third argument, if provided
+env_file="$3"
 
 # Check if the destination folder already exists
 if [ ! -d "$destination_folder" ]; then
@@ -20,36 +26,33 @@ fi
 # Change the current directory to the destination folder
 cd "$destination_folder" || exit
 
-#stash changes
+# Stash any local changes and clear stashed changes
 git stash
-
-#remove all changes
 git stash clear
 
 # Pull changes from the remote repository
 git pull
 echo "Changes pulled successfully."
 
-# Building new container image
+# Build the new container image
 echo "Building new container..."
-docker-compose build
+docker-compose build || { echo 'Docker build failed'; exit 1; }
 echo "Image built successfully."
 
-# Shutting down old instance
-echo "Removing old container"
-docker-compose down
+# Shut down the old instance
+echo "Removing old container..."
+docker-compose down || { echo 'Docker container removal failed'; exit 1; }
 echo "Old container removed successfully."
 
-# Check if the third argument is "true" to use the env file, otherwise use the regular command
-if [ "$#" -eq 3 ] && [ "$3" == "true" ]; then
-    # Use the docker-compose --env-file command
-    echo "Building new container with environment file..."
-    docker-compose --env-file *.env up -d
+# Check if the environment file is provided and exists
+if [ -n "$env_file" ] && [ -f "$env_file" ]; then
+    # Use the docker-compose with the env file
+    echo "Building new container with environment file: $env_file"
+    docker-compose --env-file "$env_file" up -d || { echo 'Docker up with env file failed'; exit 1; }
 else
     # Use the regular docker-compose up -d command
     echo "Building new container..."
-    docker-compose up -d
+    docker-compose up -d || { echo 'Docker up failed'; exit 1; }
 fi
 
 echo "Container started successfully."
-
